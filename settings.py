f764f17f407b1b8ee3db238e794ea6ebe3caf4af
toyman19 <toyman19@gmail.com>
2022-01-05 13:39:06 +0100

    makign ship displayable

--- settings.py
+++ settings.py
@@ -3,3 +3,7 @@ class Settings:
 
     def __init__(self):
         """ Initialize the settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
