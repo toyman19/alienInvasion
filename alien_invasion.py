78c9b23ac0e1991a7d9fe79c4efb1d10325b7681
toyman19 <toyman19@gmail.com>
2022-01-12 09:48:45 +0100

    Ship can move now!

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,6 +39,8 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
 
     def _update_screen(self):
